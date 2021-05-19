23b74bb0ec912615f618d2f57d30e71faff239a7
ismagaia <matheusduartec96@gmail.com>
2021-05-19 11:21:15 -0300

    Adicionado ao projeto dependencias DevTools, Flyway, Lombok, conexao e configuracao com o MySQL

--- src/main/java/com/algaworks/algalog/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/model/Cliente.java
@@ -1,12 +1,15 @@
 package com.algaworks.algalog.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Cliente {
 	private Long id;
-	private String name;
+	private String nome;
 	private String email;
 	private String telefone;
-	
-	
 	
 
 }
